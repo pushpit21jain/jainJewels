0a9759956a10b01a636dc5694410716d2dca7c1a
Pushpit Jain <jainpushpit21@gmail.com>
2026-02-18 15:47:58 +0530

    feat: Add account page and entry point script
    Introduces the account page and updates the index.html to correctly load the application entry point. Includes a new README with local development instructions.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import ProductDetail from './pages/ProductDetail';
-import Checkout from './pages/Checkout';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Admin from './pages/Admin';
+import Layout from './components/Layout.tsx';
+import Home from './pages/Home.tsx';
+import Shop from './pages/Shop.tsx';
+import ProductDetail from './pages/ProductDetail.tsx';
+import Checkout from './pages/Checkout.tsx';
+import About from './pages/About.tsx';
+import Contact from './pages/Contact.tsx';
+import Admin from './pages/Admin.tsx';
+import Account from './pages/Account.tsx';
 
 const App: React.FC = () => {
   return (
@@ -22,6 +23,7 @@ const App: React.FC = () => {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </Layout>
     </Router>
